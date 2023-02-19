59f524c8aa6844fe06d1ba484dfc1f0ebcd90b8e
Gottardy <gottgab@gmail.com>
2023-02-18 19:18:38 -0500

    validacion de eventos de conexion y desconexion

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,20 @@
-console.log('servidor corriendo')
+console.log('servidor corriendo');
+// Referencias del HTML
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
+// Socket ciente que usa la app web
+const socketClient = io();
+
+// Listeners de cambios en el cliente conectado
+socketClient.on('connect', ()=>{
+    console.log('cliente conectado del servidor');
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = ''; 
+})
+// Listeners de cambios en el cliente desconectado
+socketClient.on('disconnect', ()=>{
+    console.log('cliente desconectado del servidor');
+    lblOnline.style.display = 'none';
+    lblOffline.style.display = ''; 
+})
