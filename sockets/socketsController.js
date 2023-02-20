cb7b6c8e408fd2945be4b94a7a793c364fb99047
Gottardy <gottgab@gmail.com>
2023-02-20 11:40:56 -0500

    controlando los eventos de nuevos tickets

--- sockets/socketsController.js
+++ sockets/socketsController.js
@@ -5,14 +5,17 @@ const ticketControl = new TicketControl();
 const socketsController = (socketClient) => {
     console.log('Socket Cliente Conectado:',socketClient.id);
     
+    socketClient.emit('ultimo_ticket',ticketControl.ultimo);
+
     socketClient.on('disconnect',() => {
         console.log('Socket Cliente Desconectado:',socketClient.id);
     });
     
     // Manejo de evento de recepcion de Mensajes / Payload del cliente
     socketClient.on('notificar_mensaje', (payload, callback) =>{
-        const id = socketClient.id
-        callback(id);
+        
+        const siguienteTicket = ticketControl.nextTicket();
+        callback(siguienteTicket);
         // Notificancion del servidor a todos los clientes utilizando broadcast
         socketClient.broadcast.emit('notificar_mensaje',payload);
 
